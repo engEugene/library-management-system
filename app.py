e46f0192e378ebd3075984295209999c05db6676
sandrine dushimimana <sanrdushimimana@gmail.com>
2025-03-31 09:49:02 +0200

    Reset

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sqlite3
-from models.books import Book
 
 def create_database():
     """Creates the database and books table if they don't exist."""
@@ -19,19 +18,6 @@ def create_database():
 def menu():
    pass
 
-def createBook():
-    """ Function is called when inserting books in the db"""
-    try:
-        title = input("Enter book title: ")
-        author = input("Enter book author: ")
-        pages = int(input("Enter number of pages: "))
-
-        # Create and save the book
-        new_book = Book.create(title, author, pages)
-        print(f"Book created successfully: {new_book}")
-
-    except ValueError as e:
-        print(f"Error: {e}")
 
 def main():
     """Main function with a switch-like menu loop."""
@@ -43,7 +29,7 @@ def main():
         if choice == "1":
             pass
         elif choice == "2":
-            createBook()
+            pass
         elif choice == "3":
             print("Exiting program. Goodbye!")
             break
